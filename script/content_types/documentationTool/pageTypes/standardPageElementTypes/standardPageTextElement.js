0545965fdbe02534d82cf74dbb148267b64aa22e
Jason Benoit <jsn.benoit@gmail.com>
2025-02-02 21:51:05 -0500

    Fix element uninitialized error

--- script/content_types/documentationTool/pageTypes/standardPageElementTypes/standardPageTextElement.js
+++ script/content_types/documentationTool/pageTypes/standardPageElementTypes/standardPageTextElement.js
@@ -7,7 +7,7 @@ export default class StandardPageTextElement extends StandardPageElement {
     }
 
     getElement() {
-        
+        const element = document.createElement("div");
         if (this.pageElementContent.metadata.title == "Untitled Text") {
             const elementHeader = document.createElement("h6");
             elementHeader.style.color = "red";
@@ -17,7 +17,6 @@ export default class StandardPageTextElement extends StandardPageElement {
         
         const pageText = document.createElement("p");
         pageText.innerHTML = this.pageElementContent.params.text;
-        const element = document.createElement("div");
         element.appendChild(pageText);
         return element;
     }
