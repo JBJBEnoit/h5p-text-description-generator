7f3226f64a2799376510ab08a3f7b89d12a97064
Jason Benoit <jsn.benoit@gmail.com>
2025-02-02 21:48:37 -0500

    Fixed "Untitled Text" as element header

--- script/content_types/documentationTool/pageTypes/standardPageElementTypes/standardPageTextElement.js
+++ script/content_types/documentationTool/pageTypes/standardPageElementTypes/standardPageTextElement.js
@@ -7,12 +7,17 @@ export default class StandardPageTextElement extends StandardPageElement {
     }
 
     getElement() {
-        const elementHeader = document.createElement("h6");
-        elementHeader.textContent = this.pageElementContent.metadata.title;
+        
+        if (this.pageElementContent.metadata.title == "Untitled Text") {
+            const elementHeader = document.createElement("h6");
+            elementHeader.style.color = "red";
+            elementHeader.textContent =  this.pageElementContent.metadata.title;
+            element.appendChild(elementHeader);
+        }
+        
         const pageText = document.createElement("p");
         pageText.innerHTML = this.pageElementContent.params.text;
         const element = document.createElement("div");
-        element.appendChild(elementHeader);
         element.appendChild(pageText);
         return element;
     }
