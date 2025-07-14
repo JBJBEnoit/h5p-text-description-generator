d42c94d7781a69851305b951d2bb0ecf4c3ff5b5
Jason Benoit <jsn.benoit@gmail.com>
2025-07-14 12:54:22 -0400

    Fixed double answer lettering in multichoice

--- script/content_types/multiChoice.js
+++ script/content_types/multiChoice.js
@@ -7,6 +7,14 @@ export default class MultiChoice extends ContentType {
   }
 
   parse(contentJson) {
+    function replaceLetterPrefix(str, newPrefix) {
+      const regex = /^[a-zA-Z][\.\)]\s*/;
+      if (regex.test(str)) {
+        return str.replace(regex, newPrefix);
+      } else {
+        return newPrefix + str;
+      }
+    }
     const content = JSON.parse(contentJson);
     //console.log("MultiChoice content:", content.question);
     const questionStr = this.stripEnclosingTags(content.question.replace(/(&(?:amp;)?nbsp;)+(?=\s*<\/(li|p)>)/gi, ''));
@@ -24,13 +32,13 @@ export default class MultiChoice extends ContentType {
     answers.forEach((answer, index) => {
       //console.log("MultiChoice answer:", answer.text);
       const text = this.stripEnclosingTags(answer.text.replace(/(&(?:amp;)?nbsp;)+(?=\s*<\/(div|p|li)>)/gi, ''));
+      //console.log("MultiChoice answer text:", text);
       const li = document.createElement("li");
-      li.textContent = text;
+      li.textContent = replaceLetterPrefix(text, '');
       let correctAppended = 0;
       answerList.appendChild(li);
       if (answer.correct) {
-        correctAnswers.innerHTML +=
-          String.fromCharCode(97 + index) + ". " + text;
+        correctAnswers.innerHTML += replaceLetterPrefix(text, String.fromCharCode(97 + index) + ". ");
         if (numCorrect > 1 && correctAppended < numCorrect) {
           correctAnswers.innerHTML += "; ";
         }
